d2e029240399784147a8cf4d46730b4ee8bf906f
Robert Nelson <robertcnelson@gmail.com>
2022-10-25 14:58:07 -0500

    add u-boot
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ 03_build_uboot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ./beaglev-ahead-u-boot/
+rm -f board/thead/light-c910/version_rollback.c || true
+git reset HEAD --hard
+patch -p1 < ../patches/u-boot/0001-no-strip-fw_printenv.patch
+patch -p1 < ../patches/u-boot/0002-Add-factory-reset-env-to-uboot.patch
+patch -p1 < ../patches/u-boot/0003-LIGHT-Automatic-version-rollback-when-upgrade-fails.patch
+patch -p1 < ../patches/u-boot/0004-fix-scripts-dtc-dtc-lexer.l-for-gcc10.patch
+cd ../
+
+wdir=`pwd`
+
+make -C beaglev-ahead-u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- distclean
+make -C beaglev-ahead-u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- light_beagle_defconfig
+make -C beaglev-ahead-u-boot -j8 ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- all
+
+cp -v ./beaglev-ahead-u-boot/u-boot-with-spl.bin ./deploy/
+
+cd ./beaglev-ahead-u-boot/
+rm -f board/thead/light-c910/version_rollback.c || true
+git reset HEAD --hard
+cd ../
+
