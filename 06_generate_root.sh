3c453075596625483dfa0f315665ccd5dabe5dd5
Robert Nelson <robertcnelson@gmail.com>
2022-10-25 17:35:37 -0500

    06_generate_root.sh: modules added a bug, disable
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 06_generate_root.sh
+++ 06_generate_root.sh
@@ -13,10 +13,17 @@ fi
 
 mkdir -p ./ignore/.root
 
-sudo tar xfvp ./deploy/debian-sid-console-riscv64-${image}/riscv64-rootfs-*.tar -C ./ignore/.root
+sudo tar xfp ./deploy/debian-sid-console-riscv64-${image}/riscv64-rootfs-*.tar -C ./ignore/.root
 sync
 
-dd if=/dev/zero of=./deploy/root.ext4 bs=1 count=0 seek=2G
+#if [ -f ./deploy/.modules ] ; then
+#	version=$(cat ./deploy/.modules || true)
+#	if [ -f ./deploy/${version}.tar.gz ] ; then
+#		sudo tar xfv ./deploy/${version}.tar.gz -C ./ignore/.root
+#	fi
+#fi
+
+dd if=/dev/zero of=./deploy/root.ext4 bs=1 count=0 seek=2000M
 sudo mkfs.ext4 -F ./deploy/root.ext4 -d ./ignore/.root
 
 cd ../
