9b67f8a48e265ca9379e7006a761d263669c8209
Robert Nelson <robertcnelson@gmail.com>
2022-10-26 21:24:37 -0500

    rootfs: use a microSD
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 07_fastboot_emmc.sh
+++ 07_fastboot_emmc.sh
@@ -5,5 +5,4 @@ sudo fastboot reboot
 sleep 10
 sudo fastboot flash uboot ./deploy/u-boot-with-spl.bin
 sudo fastboot flash boot ./deploy/boot.ext4
-sudo fastboot flash root ./deploy/root.ext4
 sudo fastboot reboot
