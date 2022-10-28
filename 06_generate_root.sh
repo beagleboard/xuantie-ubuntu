cf68c0b2f2554c9533c610c415cf5f151771b987
Robert Nelson <robertcnelson@gmail.com>
2022-10-28 10:03:18 -0500

    ci: cleanup 06_generate_root.sh
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 06_generate_root.sh
+++ 06_generate_root.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! id | grep -q root; then
+	echo "./06_generate_root.sh must be run as root:"
+	echo "sudo ./06_generate_root.sh"
+	exit
+fi
+
 wdir=`pwd`
 
 image="2022-10-26"
@@ -12,32 +18,31 @@ if [ ! -f ./deploy/debian-sid-console-riscv64-${image}/riscv64-rootfs-debian-sid
 fi
 
 if [ -d ./ignore/.root ] ; then
-	sudo rm -rf ./ignore/.root || true
+	rm -rf ./ignore/.root || true
 fi
-sudo mkdir -p ./ignore/.root
+mkdir -p ./ignore/.root
 
-sudo tar xfp ./deploy/debian-sid-console-riscv64-${image}/riscv64-rootfs-*.tar -C ./ignore/.root
+tar xfp ./deploy/debian-sid-console-riscv64-${image}/riscv64-rootfs-*.tar -C ./ignore/.root
 sync
 
-sudo mkdir -p ./ignore/.root/boot/firmware/ || true
+mkdir -p ./ignore/.root/boot/firmware/ || true
 
-sudo sh -c "echo '/dev/mmcblk0p2  /boot/firmware/ auto  defaults  0  2' >> ./ignore/.root/etc/fstab"
-sudo sh -c "echo '/dev/mmcblk0p3  /  auto  errors=remount-ro  0  1' >> ./ignore/.root/etc/fstab"
+echo '/dev/mmcblk0p2  /boot/firmware/ auto  defaults  0  2' >> ./ignore/.root/etc/fstab
+echo '/dev/mmcblk0p3  /  auto  errors=remount-ro  0  1' >> ./ignore/.root/etc/fstab
 
-sudo rm -rf ./ignore/.root/usr/lib/modules/5.13.6-riscv64-r17/ || true
+rm -rf ./ignore/.root/usr/lib/modules/5.13.6-riscv64-r17/ || true
 
-sudo rm -rf ./ignore/.root/usr/lib/systemd/system/bb-usb-gadgets.service || true
-sudo rm -rf ./ignore/.root/usr/lib/systemd/system/grow_partition.service || true
+rm -rf ./ignore/.root/usr/lib/systemd/system/bb-usb-gadgets.service || true
+rm -rf ./ignore/.root/usr/lib/systemd/system/grow_partition.service || true
 
 if [ -f ./deploy/.modules ] ; then
 	version=$(cat ./deploy/.modules || true)
 	if [ -f ./deploy/${version}.tar.gz ] ; then
-		sudo tar xfv ./deploy/${version}.tar.gz -C ./ignore/.root/usr/
+		tar xfv ./deploy/${version}.tar.gz -C ./ignore/.root/usr/
 	fi
 fi
 
 dd if=/dev/zero of=./deploy/root.ext4 bs=1 count=0 seek=1500M
-sudo mkfs.ext4 -F ./deploy/root.ext4 -d ./ignore/.root
+mkfs.ext4 -F ./deploy/root.ext4 -d ./ignore/.root
 
 cd ../
-
