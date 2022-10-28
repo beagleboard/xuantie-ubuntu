917eca5eedde824035a164bdfb46b100f919c50f
Robert Nelson <robertcnelson@gmail.com>
2022-10-28 09:56:55 -0500

    05_generate_boot.sh: should run as root in ci
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 05_generate_boot.sh
+++ 05_generate_boot.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! id | grep -q root; then
+	echo "./05_generate_boot.sh must be run as root:"
+	echo "sudo ./05_generate_boot.sh"
+	exit
+fi
+
 mkdir -p ./ignore/.boot
 cp -v ./deploy/fw_dynamic.bin ./ignore/.boot
 cp -v ./bins/light_aon_fpga.bin ./ignore/.boot
@@ -8,4 +14,4 @@ cp -v ./deploy/Image ./ignore/.boot
 cp -v ./deploy/light-beagle.dtb ./ignore/.boot
 
 dd if=/dev/zero of=./deploy/boot.ext4 count=10000 bs=4096
-sudo mkfs.ext4 -F ./deploy/boot.ext4 -d ./ignore/.boot
+mkfs.ext4 -F ./deploy/boot.ext4 -d ./ignore/.boot
