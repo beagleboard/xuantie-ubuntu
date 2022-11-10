40286ba510ceab257afec9143952c0379efd8470
Robert Nelson <robertcnelson@gmail.com>
2022-11-10 17:49:03 -0600

    git: i hate submodules, going to convert this later
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 01_git_sync.sh
+++ 01_git_sync.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-git reset HEAD --hard
+if [ -d ./beaglev-ahead-linux ] ; then
+	cd ./beaglev-ahead-linux/
+	git reset HEAD --hard
+	cd ../
+fi
+
 git submodule init && git submodule sync && git submodule update --remote
