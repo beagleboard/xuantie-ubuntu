b6ac6528da576bd823989a758c532d3b1f9dfcdc
Robert Nelson <robertcnelson@gmail.com>
2022-11-09 20:00:52 -0600

    u-boot: move 0003-LIGHT-Automatic-version-rollback-when-upgrade-fails.patch and 0004-fix-scripts-dtc-dtc-lexer.l-for-gcc10.patch u-boot branch
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 03_build_uboot.sh
+++ 03_build_uboot.sh
@@ -3,10 +3,7 @@
 CORES=$(getconf _NPROCESSORS_ONLN)
 
 cd ./beaglev-ahead-u-boot/
-rm -f board/thead/light-c910/version_rollback.c || true
 git reset HEAD --hard
-patch -p1 < ../patches/u-boot/0003-LIGHT-Automatic-version-rollback-when-upgrade-fails.patch
-patch -p1 < ../patches/u-boot/0004-fix-scripts-dtc-dtc-lexer.l-for-gcc10.patch
 
 cp -v ../patches/u-boot/light-c910.h ./include/configs/
 
@@ -23,7 +20,6 @@ make -C beaglev-ahead-u-boot -j${CORES} ARCH=riscv CROSS_COMPILE=${wdir}/riscv-t
 cp -v ./beaglev-ahead-u-boot/u-boot-with-spl.bin ./deploy/
 
 cd ./beaglev-ahead-u-boot/
-rm -f board/thead/light-c910/version_rollback.c || true
 git reset HEAD --hard
 cd ../
 
