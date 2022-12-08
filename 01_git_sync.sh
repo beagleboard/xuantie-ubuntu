dc7742f8008f6bf485e84af29d20587fa7a4f2af
Robert Nelson <robertcnelson@gmail.com>
2022-12-08 12:43:25 -0600

    01_git_sync.sh: reorder
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 01_git_sync.sh
+++ 01_git_sync.sh
@@ -8,14 +8,16 @@ fi
 
 git submodule init && git submodule sync && git submodule update --remote
 
+if [ -d ./riscv-toolchain ] ; then
+	rm -rf ./riscv-toolchain || true
+fi
+
+git clone git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
+
 if [ -d ./opensbi ] ; then
 	rm -rf ./opensbi || true
 fi
 
 git clone git@git.beagleboard.org:beaglev-ahead/opensbi.git --depth=1
 
-if [ -d ./riscv-toolchain ] ; then
-	rm -rf ./riscv-toolchain || true
-fi
-
-git clone git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
+#
