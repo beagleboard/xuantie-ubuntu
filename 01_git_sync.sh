ef902694cee6fb5e7d55fd2baab874254d09ee20
Robert Nelson <robertcnelson@gmail.com>
2022-12-08 12:41:00 -0600

    riscv-toolchain: remove git submodule
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 01_git_sync.sh
+++ 01_git_sync.sh
@@ -12,4 +12,10 @@ if [ -d ./opensbi ] ; then
 	rm -rf ./opensbi || true
 fi
 
-git clone git@git.beagleboard.org:beaglev-ahead/opensbi.git
+git clone git@git.beagleboard.org:beaglev-ahead/opensbi.git --depth=1
+
+if [ -d ./riscv-toolchain ] ; then
+	rm -rf ./riscv-toolchain || true
+fi
+
+git clone git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
