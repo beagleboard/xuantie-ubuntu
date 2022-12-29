bd3fcc3c0f9552e67b39ca34ca556797f1c07dc6
Robert Nelson <robertcnelson@gmail.com>
2022-12-28 18:11:00 -0600

    ci: use local git reference
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 01_git_sync.sh
+++ 01_git_sync.sh
@@ -3,24 +3,43 @@
 if [ -d ./riscv-toolchain ] ; then
 	rm -rf ./riscv-toolchain || true
 fi
-
-git clone git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
+if [ -f ./.gitlab-runner ] ; then
+	git clone --reference-if-able /mnt/yocto-cache/git/riscv-toolchain/ git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
+else
+	git clone git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
+fi
 
 if [ -d ./opensbi ] ; then
 	rm -rf ./opensbi || true
 fi
 
-git clone git@git.beagleboard.org:beaglev-ahead/opensbi.git --depth=1
+if [ -f ./.gitlab-runner ] ; then
+	git clone --reference-if-able /mnt/yocto-cache/git/opensbi/ git@git.beagleboard.org:beaglev-ahead/opensbi.git --depth=1
+else
+	git clone git@git.beagleboard.org:beaglev-ahead/opensbi.git --depth=10
+fi
 
 if [ -d ./beaglev-ahead-u-boot ] ; then
 	rm -rf ./beaglev-ahead-u-boot || true
 fi
 
-git clone -b beaglev-ahead-v2020.01-0.9.5 git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-u-boot.git --depth=10
+if [ -f ./.gitlab-runner ] ; then
+	git clone --reference-if-able /mnt/yocto-cache/git/beaglev-ahead-u-boot/ -b beaglev-ahead-v2020.01-0.9.5 git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-u-boot.git --depth=1
+else
+	git clone -b beaglev-ahead-v2020.01-0.9.5 git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-u-boot.git --depth=10
+fi
 
 if [ -d ./beaglev-ahead-linux ] ; then
 	rm -rf ./beaglev-ahead-linux || true
 fi
 
-git clone -b beaglev-ahead-v5.10.113-0.9.5 git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-linux.git --depth=10
+if [ -f ./.gitlab-runner ] ; then
+	git clone --reference-if-able /mnt/yocto-cache/git/beaglev-ahead-linux/ -b beaglev-ahead-v5.10.113-0.9.5 git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-linux.git --depth=1
+else
+	git clone -b beaglev-ahead-v5.10.113-0.9.5 git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-linux.git --depth=10
+fi
+
+if [ -f ./.gitlab-runner ] ; then
+	rm -f ./.gitlab-runner || true
+fi
 #
