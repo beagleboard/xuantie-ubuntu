be7867c88914a5c445e9ee17d23b335bb49c0c75
Robert Nelson <robertcnelson@gmail.com>
2023-02-14 11:12:08 -0600

    boot: add prototype extlinux.conf file
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 05_generate_boot.sh
+++ 05_generate_boot.sh
@@ -13,6 +13,13 @@ cp -v ./bins/light_c906_audio.bin ./ignore/.boot
 cp -v ./deploy/Image ./ignore/.boot
 cp -v ./deploy/light-beagle.dtb ./ignore/.boot
 
+mkdir ./ignore/.boot/extlinux/
+echo "label Linux eMMC" > ./ignore/.boot/extlinux/extlinux.conf
+echo "    kernel /Image" >> ./ignore/.boot/extlinux/extlinux.conf
+echo "    append root=/dev/mmcblk0p3 ro rootfstype=ext4 rootwait console=ttyS0,115200 earlycon clk_ignore_unused net.ifnames=0" >> ./ignore/.boot/extlinux/extlinux.conf
+echo "    fdtdir /" >> ./ignore/.boot/extlinux/extlinux.conf
+echo "    #fdtoverlays /overlays/<file>.dtbo" >> ./ignore/.boot/extlinux/extlinux.conf
+
 dd if=/dev/zero of=./deploy/boot.ext4 bs=1 count=0 seek=190M
 mkfs.ext4 -F ./deploy/boot.ext4 -d ./ignore/.boot
 
