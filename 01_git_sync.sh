528c1061e4d5a16f6cffb9a45656291bc01a5f32
Robert Nelson <robertcnelson@gmail.com>
2023-03-29 16:59:20 -0500

    01_git_sync.sh: cleanup some legacy directories
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 01_git_sync.sh
+++ 01_git_sync.sh
@@ -7,12 +7,8 @@ if [ -f ./.gitlab-runner ] ; then
 	git clone --reference-if-able /mnt/yocto-cache/git/riscv-toolchain/ git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
 else
 	if [ ! -d ./riscv-toolchain ] ; then
-		if [ -d ./mirror/riscv-toolchain ] ; then
-			git clone --reference-if-able ./mirror/riscv-toolchain git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
-		else
-			echo "Log riscv-toolchain: [git clone git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1]"
-			git clone git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
-		fi
+		echo "Log riscv-toolchain: [git clone git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1]"
+		git clone git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
 	else
 		cd ./riscv-toolchain/
 		echo "Log riscv-toolchain: [git pull --rebase]"
@@ -35,10 +31,6 @@ else
 	fi
 fi
 
-if [ -d ./beaglev-ahead-u-boot ] ; then
-	rm -rf ./beaglev-ahead-u-boot || true
-fi
-
 if [ -d ./u-boot ] ; then
 	rm -rf ./u-boot || true
 fi
@@ -47,10 +39,6 @@ if [ -f ./.gitlab-runner ] ; then
 	git clone --reference-if-able /mnt/yocto-cache/git/beaglev-ahead-u-boot/ -b ${UBOOT_BRANCH} git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-u-boot.git ./u-boot/ --depth=1
 else
 	git clone -b ${UBOOT_BRANCH} git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-u-boot.git ./u-boot/ --depth=10
-fi
-
-if [ -d ./beaglev-ahead-linux ] ; then
-	rm -rf ./beaglev-ahead-linux || true
 fi
 
 if [ -d ./BeagleBoard-DeviceTrees ] ; then
