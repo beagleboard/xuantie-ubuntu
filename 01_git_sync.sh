ae57c14ad6545014f39770b07d2ec63f63c5fd93
Robert Nelson <robertcnelson@gmail.com>
2023-04-27 13:20:38 -0500

    toolchain: drop git repo, we will dl the file when going public
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>
    
    Former-commit-id: 4d9a3cbdfb6e01b27b57c658dbf0e38b065e93df

--- 01_git_sync.sh
+++ 01_git_sync.sh
@@ -3,21 +3,19 @@
 UBOOT_BRANCH="beaglev-v2020.01-1.0.3"
 LINUX_BRANCH="beaglev-v5.10.113-1.0.3"
 
-if [ -f ./.gitlab-runner ] ; then
-	git clone --reference-if-able /mnt/yocto-cache/git/riscv-toolchain/ git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
-else
-	if [ ! -d ./riscv-toolchain ] ; then
-		echo "Log riscv-toolchain: [git clone git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1]"
-		git clone git@git.beagleboard.org:beaglev-ahead/riscv-toolchain.git --depth=1
-	else
-		cd ./riscv-toolchain/
-		echo "Log riscv-toolchain: [git pull --rebase]"
-		git pull --rebase
-		cd -
-	fi
+if [ ! -f ./mirror/Xuantie-900-gcc-linux-5.10.4-glibc-x86_64-V2.2.8.tar.xz ] ; then
+	###FIXME, move to public when released...
+	echo "wget -c https://git.beagleboard.org/beaglev-ahead/xuantie-tools/-/raw/main/Xuantie-900-gcc-linux-5.10.4-glibc-x86_64-V2.2.8.tar.xz"
+	wget -c https://git.beagleboard.org/beaglev-ahead/xuantie-tools/-/raw/main/Xuantie-900-gcc-linux-5.10.4-glibc-x86_64-V2.2.8.tar.xz
+fi
+
+if [ ! -f ./riscv-toolchain/README.md ] ; then
+	echo "tar xf ./mirror/Xuantie-900-gcc-linux-5.10.4-glibc-x86_64-V2.2.8.tar.xz -C ./riscv-toolchain/"
+	tar xf ./mirror/Xuantie-900-gcc-linux-5.10.4-glibc-x86_64-V2.2.8.tar.xz -C ./riscv-toolchain/
 fi
 
 if [ -f ./.gitlab-runner ] ; then
+	echo "git clone --reference-if-able /mnt/yocto-cache/git/opensbi/ git@git.beagleboard.org:beaglev-ahead/opensbi.git --depth=1"
 	git clone --reference-if-able /mnt/yocto-cache/git/opensbi/ git@git.beagleboard.org:beaglev-ahead/opensbi.git --depth=1
 else
 	if [ ! -d ./opensbi ] ; then
@@ -36,8 +34,10 @@ if [ -d ./u-boot ] ; then
 fi
 
 if [ -f ./.gitlab-runner ] ; then
+	echo "git clone --reference-if-able /mnt/yocto-cache/git/beaglev-ahead-u-boot/ -b ${UBOOT_BRANCH} git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-u-boot.git ./u-boot/ --depth=1"
 	git clone --reference-if-able /mnt/yocto-cache/git/beaglev-ahead-u-boot/ -b ${UBOOT_BRANCH} git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-u-boot.git ./u-boot/ --depth=1
 else
+	echo "git clone -b ${UBOOT_BRANCH} git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-u-boot.git ./u-boot/ --depth=10"
 	git clone -b ${UBOOT_BRANCH} git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-u-boot.git ./u-boot/ --depth=10
 fi
 
@@ -45,6 +45,7 @@ if [ -d ./BeagleBoard-DeviceTrees ] ; then
 	rm -rf ./BeagleBoard-DeviceTrees || true
 fi
 
+echo "git clone -b v5.10.x-ti-unified git@git.beagleboard.org:beaglev-ahead/BeagleBoard-DeviceTrees.git"
 git clone -b v5.10.x-ti-unified git@git.beagleboard.org:beaglev-ahead/BeagleBoard-DeviceTrees.git
 
 if [ -d ./linux ] ; then
@@ -52,8 +53,10 @@ if [ -d ./linux ] ; then
 fi
 
 if [ -f ./.gitlab-runner ] ; then
+	echo "git clone --reference-if-able /mnt/yocto-cache/git/beaglev-ahead-linux/ -b ${LINUX_BRANCH} git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-linux.git ./linux/ --depth=1"
 	git clone --reference-if-able /mnt/yocto-cache/git/beaglev-ahead-linux/ -b ${LINUX_BRANCH} git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-linux.git ./linux/ --depth=1
 else
+	echo "git clone -b ${LINUX_BRANCH} git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-linux.git ./linux/ --depth=10"
 	git clone -b ${LINUX_BRANCH} git@git.beagleboard.org:beaglev-ahead/beaglev-ahead-linux.git ./linux/ --depth=10
 fi
 
--- 03_build_uboot.sh
+++ 03_build_uboot.sh
@@ -4,18 +4,18 @@ CORES=$(getconf _NPROCESSORS_ONLN)
 
 wdir=`pwd`
 
-make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- distclean
+make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-linux- distclean
 
-#make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- light_beagle_defconfig
-#make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- menuconfig
-#make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- savedefconfig
+#make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-linux- light_beagle_defconfig
+#make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-linux- menuconfig
+#make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-linux- savedefconfig
 #cp -v ./u-boot/defconfig ./u-boot/configs/light_beagle_defconfig
-#make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- distclean
+#make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-linux- distclean
 
-echo "make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- light_beagle_defconfig"
-make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- light_beagle_defconfig
-echo "make -C u-boot -j${CORES} ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- all"
-make -C u-boot -j${CORES} ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/riscv-toolchain/bin/riscv64-linux- all
+echo "make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-linux- light_beagle_defconfig"
+make -C u-boot ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-linux- light_beagle_defconfig
+echo "make -C u-boot -j${CORES} ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-linux- all"
+make -C u-boot -j${CORES} ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-linux- all
 
 cp -v ./u-boot/u-boot-with-spl.bin ./deploy/
 
