fd52c137d5419ccfa3d429fa446530909978897a
Robert Nelson <robertcnelson@gmail.com>
2023-05-05 22:26:20 -0500

    rootfs: pick up the new location of hte gpu modules
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>
    
    Former-commit-id: 0b914257cb57affba18988647d02be9bf5d818ec

--- 07_generate_ubuntu_xfce_root.sh
+++ 07_generate_ubuntu_xfce_root.sh
@@ -95,8 +95,8 @@ if [ -f ./deploy/.modules ] ; then
 fi
 
 mkdir -p ./ignore/.root/usr/lib/modules/${version}/extra/
-cp -v ./binary_thead_linux_lws-generic_release/target_riscv64/kbuild/drm_nulldisp.ko ./ignore/.root/usr/lib/modules/${version}/extra/
-cp -v ./binary_thead_linux_lws-generic_release/target_riscv64/kbuild/pvrsrvkm.ko ./ignore/.root/usr/lib/modules/${version}/extra/
+cp -v ./gpu_bxm_4_64-kernel/rogue_km/binary_thead_linux_lws-generic_release/target_riscv64/kbuild/drm_nulldisp.ko ./ignore/.root/usr/lib/modules/${version}/extra/
+cp -v ./gpu_bxm_4_64-kernel/rogue_km/binary_thead_linux_lws-generic_release/target_riscv64/kbuild/pvrsrvkm.ko ./ignore/.root/usr/lib/modules/${version}/extra/
 cp -v ./vi-kernel/output/rootfs/bsp/isp/ko/*.ko ./ignore/.root/usr/lib/modules/${version}/extra/
 cp -v ./baremetal-drivers/output/rootfs/bsp/baremetal/ko/*.ko ./ignore/.root/usr/lib/modules/${version}/extra/
 cp -v ./video_memory/output/rootfs/bsp/vidmem/ko/*.ko ./ignore/.root/usr/lib/modules/${version}/extra/
