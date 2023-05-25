64a860cbcb94ae1ac376815df357376a95429b4d
Robert Nelson <robertcnelson@gmail.com>
2023-05-25 17:55:07 -0500

    bump rootfs: 2023-05-25, ubuntu, preinstall evdev thru pip
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>
    
    Former-commit-id: 5f623d6ffa7660df35e24d38c6d43e515fea1c36

--- 07_generate_root.sh
+++ 07_generate_root.sh
@@ -8,7 +8,7 @@ fi
 
 wdir=`pwd`
 
-image="2023-05-20"
+image="2023-05-25"
 
 if [ ! -f ./deploy/debian-sid-console-riscv64-${image}/riscv64-rootfs-debian-sid.tar ] ; then
 	wget -c --directory-prefix=./deploy https://rcn-ee.net/rootfs/debian-riscv64-minimal/${image}/debian-sid-console-riscv64-${image}.tar.xz
