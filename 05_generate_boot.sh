0bd9e78dd557f63d6df6096aa9b5a9e2f3dc7b63
Robert Nelson <robertcnelson@gmail.com>
2023-07-23 17:11:08 -0500

    boot: copy overlays from linux and echo extlinux.conf setup
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 05_generate_boot.sh
+++ 05_generate_boot.sh
@@ -17,8 +17,10 @@ fi
 cp -v ./deploy/Image ./ignore/.boot
 cp -v ./deploy/light-beagle.dtb ./ignore/.boot
 
-mkdir -p ./ignore/.boot/overlays/ || true
-cp -v ./BeagleBoard-DeviceTrees/src/riscv/overlays/*.dtbo ./ignore/.boot/overlays/
+if [ -d ./linux/arch/riscv/boot/dts/thead/overlays/ ] ; then
+	mkdir -p ./ignore/.boot/overlays/ || true
+	cp -v ./linux/arch/riscv/boot/dts/thead/overlays/*.dtbo ./ignore/.boot/overlays/
+fi
 
 mkdir -p ./ignore/.boot/extlinux/ || true
 echo "label Linux eMMC" > ./ignore/.boot/extlinux/extlinux.conf
@@ -26,6 +28,9 @@ echo "    kernel /Image" >> ./ignore/.boot/extlinux/extlinux.conf
 echo "    append root=/dev/mmcblk0p3 ro rootfstype=ext4 rootwait console=ttyS0,115200 earlycon clk_ignore_unused net.ifnames=0" >> ./ignore/.boot/extlinux/extlinux.conf
 echo "    fdtdir /" >> ./ignore/.boot/extlinux/extlinux.conf
 echo "    #fdtoverlays /overlays/<file>.dtbo" >> ./ignore/.boot/extlinux/extlinux.conf
+
+echo "extlinux/extlinux.conf"
+cat ./ignore/.boot/extlinux/extlinux.conf
 
 dd if=/dev/zero of=./deploy/boot.ext4 bs=1 count=0 seek=190M
 mkfs.ext4 -F ./deploy/boot.ext4 -d ./ignore/.boot
