89b5743b29d697ba232166ca7aca4378abbdf1c9
Robert Nelson <robertcnelson@gmail.com>
2023-07-23 17:13:46 -0500

    ci: mac users add script
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ deploy/mac_fastboot_emmc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#https://forum.beagleboard.org/t/flashing-beaglev-from-osx-m2-mac/35405
+#brew install --cask android-platform-tools
+
+if [ -f boot.ext4.xz ] ; then
+	unxz -v boot.ext4.xz
+fi
+
+if [ -f root.ext4.xz ] ; then
+	unxz -v root.ext4.xz
+fi
+
+fastboot flash ram ./u-boot-with-spl.bin
+fastboot reboot
+sleep 10
+fastboot oem format
+fastboot flash uboot ./u-boot-with-spl.bin
+fastboot flash boot ./boot.ext4
+fastboot flash root ./root.ext4
+fastboot reboot
