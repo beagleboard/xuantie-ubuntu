f7790683359861d2cb6e4df1d495613799aa7bb7
Robert Nelson <robertcnelson@gmail.com>
2023-07-28 16:12:54 -0500

    gcc: allow users to overide CC
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 02_build_opensbi.sh
+++ 02_build_opensbi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 CORES=$(getconf _NPROCESSORS_ONLN)
-
 wdir=`pwd`
+CC=${CC:-"${wdir}/riscv-toolchain/bin/riscv64-unknown-linux-gnu-"}
 
-make -C opensbi ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-unknown-linux-gnu- PLATFORM=generic clean
-echo "make -C opensbi -j${CORES} ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-unknown-linux-gnu- PLATFORM=generic"
-make -C opensbi -j${CORES} ARCH=riscv CROSS_COMPILE=${wdir}/riscv-toolchain/bin/riscv64-unknown-linux-gnu- PLATFORM=generic
+make -C opensbi ARCH=riscv CROSS_COMPILE=${CC} PLATFORM=generic clean
+echo "make -C opensbi -j${CORES} ARCH=riscv CROSS_COMPILE=${CC} PLATFORM=generic"
+make -C opensbi -j${CORES} ARCH=riscv CROSS_COMPILE=${CC} PLATFORM=generic
 
 cp -v ./opensbi/build/platform/generic/firmware/fw_dynamic.bin ./deploy/
 
