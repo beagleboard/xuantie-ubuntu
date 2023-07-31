4e289ec5937267575fbdafd79b13473fddbd3f63
Robert Nelson <robertcnelson@gmail.com>
2023-07-31 13:30:50 -0500

    ubuntu: disable snapd
    fixes: https://github.com/beagleboard/xuantie-ubuntu/commit/9cebf7f80fb472cb157fa06f421bc6fdfe88db60
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 06_generate_ubuntu_xfce_root.sh
+++ 06_generate_ubuntu_xfce_root.sh
@@ -41,6 +41,7 @@ echo '/dev/mmcblk0p2  /boot/firmware/ auto  defaults  0  2' >> ./ignore/.root/et
 echo '/dev/mmcblk0p3  /  auto  errors=remount-ro  0  1' >> ./ignore/.root/etc/fstab
 echo 'debugfs  /sys/kernel/debug  debugfs  mode=755,uid=root,gid=gpio,defaults  0  0' >> ./ignore/.root/etc/fstab
 
+rm -rf ./ignore/.root/usr/lib/systemd/system/snapd.service || true
 rm -rf ./ignore/.root/usr/lib/systemd/system/bb-usb-gadgets.service || true
 rm -rf ./ignore/.root/usr/lib/systemd/system/grow_partition.service || true
 cd ./ignore/.root/
