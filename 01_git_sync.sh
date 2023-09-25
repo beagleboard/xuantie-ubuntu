a13674b8b4303909792445015b0c332d9c8f888a
Robert Nelson <robertcnelson@gmail.com>
2023-09-25 15:57:03 -0500

    v6.6-rc3: mmc with DMA
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 01_git_sync.sh
+++ 01_git_sync.sh
@@ -17,8 +17,18 @@ LINUX_BRANCH="master"
 #LINUX_REPO="https://github.com/beagleboard/linux.git"
 LINUX_REPO="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
 
-LINUX_BBBIO_NEXT_BRANCH="v6.6-rc1-for-next"
-LINUX_BBBIO_BRANCH="v6.6-rc2-BeagleV-Ahead"
+#git pull --no-edit https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master
+#git pull --no-edit https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master --tags
+#git push origin master
+#git fetch https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master --tags
+#git push origin master --tags
+#git checkout v6.6-rc3 -b v6.6-rc3-BeagleV-Ahead
+##git pull --no-edit git://git.kernel.org/pub/scm/linux/kernel/git/palmer/linux.git for-next --rebase
+#git pull --no-edit https://git.beagleboard.org/beaglev-ahead/linux.git v6.6-rc2-BeagleV-Ahead --no-rebase
+#git rebase v6.6-rc3
+#git push origin v6.6-rc3-BeagleV-Ahead
+
+LINUX_BBBIO_BRANCH="v6.6-rc3-BeagleV-Ahead"
 LINUX_BBBIO_REPO="https://git.beagleboard.org/beaglev-ahead/linux.git"
 
 if [ ! -f ./mirror/x86_64-gcc-${GCC_VERSION}-nolibc-riscv64-linux.tar.xz ] ; then
@@ -65,15 +75,8 @@ if [ -f ./.gitlab-runner ] ; then
 	echo "git clone --reference-if-able /mnt/yocto-cache/git/linux-src/ -b ${LINUX_BRANCH} ${LINUX_REPO} ./linux/"
 	git clone --reference-if-able /mnt/yocto-cache/git/linux-src/ -b ${LINUX_BRANCH} ${LINUX_REPO} ./linux/
 else
-	echo "git clone -b ${LINUX_BRANCH} ${LINUX_REPO} ./linux/ --depth=200"
-	git clone -b ${LINUX_BRANCH} ${LINUX_REPO} ./linux/ --depth=200
-fi
-
-if [ "${LINUX_BBBIO_NEXT_BRANCH}" ] ; then
-	cd ./linux/
-		echo "git pull --no-edit ${LINUX_BBBIO_REPO} ${LINUX_BBBIO_NEXT_BRANCH} --no-rebase"
-		git pull --no-edit ${LINUX_BBBIO_REPO} ${LINUX_BBBIO_NEXT_BRANCH} --no-rebase
-	cd ../
+	echo "git clone -b ${LINUX_BRANCH} ${LINUX_REPO} ./linux/ --depth=${GIT_DEPTH}"
+	git clone -b ${LINUX_BRANCH} ${LINUX_REPO} ./linux/ --depth=${GIT_DEPTH}
 fi
 
 if [ "${LINUX_BBBIO_BRANCH}" ] ; then
