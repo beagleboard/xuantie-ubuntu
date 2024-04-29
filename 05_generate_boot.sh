fc2811983096f0550e94990dcbdf4ed4251a82ef
Robert Nelson <robertcnelson@gmail.com>
2024-04-29 17:55:51 -0500

    image: use new cloud storage from hetzner
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 05_generate_boot.sh
+++ 05_generate_boot.sh
@@ -33,6 +33,10 @@ echo "    #fdtoverlays /overlays/<file>.dtbo" >> ./ignore/.boot/extlinux/extlinu
 echo "extlinux/extlinux.conf"
 cat ./ignore/.boot/extlinux/extlinux.conf
 
+if [ -f ./sysconf.txt ] ; then
+	cp -v ./sysconf.txt ./ignore/.boot/
+fi
+
 dd if=/dev/zero of=./deploy/boot.ext4 bs=1 count=0 seek=190M
 mkfs.ext4 -F ./deploy/boot.ext4 -d ./ignore/.boot
 
