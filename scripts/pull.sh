cd8a7a3ed5782126dd7e52d1b64093a7af77cd45
Robert Nelson <robertcnelson@gmail.com>
2025-05-13 17:01:42 -0500

    ci: move and document mirrors
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ scripts/pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git pull --rebase --no-edit https://github.com/beagleboard/xuantie-ubuntu.git v6.13.x
+git pull --rebase --no-edit https://openbeagle.org/beaglev-ahead/xuantie-ubuntu.git v6.13.x
+git pull --rebase --no-edit https://gitlab.com/RobertCNelson/xuantie-ubuntu.git v6.13.x
