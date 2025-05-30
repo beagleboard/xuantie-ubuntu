4a6f7055095eb90224a50f77d993c5a5803bbbec
Robert Nelson <robertcnelson@gmail.com>
2025-05-30 09:35:45 -0500

    fix puller
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git pull --rebase --no-edit https://github.com/beagleboard/xuantie-ubuntu.git master
-git pull --rebase --no-edit https://openbeagle.org/beaglev-ahead/xuantie-ubuntu.git master
-git pull --rebase --no-edit https://gitlab.com/beagle-rootfs/xuantie-ubuntu.git master
+git pull --rebase --no-edit https://github.com/beagleboard/xuantie-ubuntu.git v6.11.x
+git pull --rebase --no-edit https://openbeagle.org/beaglev-ahead/xuantie-ubuntu.git v6.11.x
+git pull --rebase --no-edit https://gitlab.com/beagle-rootfs/xuantie-ubuntu.git v6.11.x
