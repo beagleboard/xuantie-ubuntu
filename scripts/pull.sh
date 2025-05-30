0f89a3b75525e3c826ddc595e05bbb304670f36a
Robert Nelson <robertcnelson@gmail.com>
2025-05-30 09:33:57 -0500

    ci: add puller
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ scripts/pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git pull --rebase --no-edit https://github.com/beagleboard/xuantie-ubuntu.git master
+git pull --rebase --no-edit https://openbeagle.org/beaglev-ahead/xuantie-ubuntu.git master
+git pull --rebase --no-edit https://gitlab.com/beagle-rootfs/xuantie-ubuntu.git master
